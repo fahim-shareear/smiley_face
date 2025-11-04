3f0a23ff253f189c045fe559a2e7505451b393ab
arctic-shark <fahimshareear@gmail.com>
2025-11-04 06:03:21 +0600

    Removing Shadow

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const rightEye = document.getElementById('two');
 // Movement limits (so pupils don't go too far)
 const PUPIL_RANGE = 30; // pixels
 
-document.addEventListener('click', (e) => {
+document.addEventListener('mousemove', (e) => {
     // Get eye positions
     const leftRect = leftEye.getBoundingClientRect();
     const rightRect = rightEye.getBoundingClientRect();
